9c36521aca46f919417489f58b55606cdd422f02
matthew harwood <matthhar12@gmail.com>
2023-01-21 23:47:00 +0000

    Updated errors

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,4 +1,4 @@
-const shuffle = <Type extends unknown>(arr: Array<Type>): Array<Type> => {
+const shuffle = (arr: any): any => {
     const array = [...arr];
     for (let i = array.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
