5dcaaf9f31cf77153110bcdf048076241396f0fd
matthew harwood <matthhar12@gmail.com>
2023-01-22 02:17:52 +0000

    added builder

---
+++ src/routes/bulider.tsx
@@ -0,0 +1,34 @@
+import { component$, Resource, useResource$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
+import { getContent, RenderContent, getBuilderSearchParams } from "@builder.io/sdk-qwik";
+
+export const BUILDER_PUBLIC_API_KEY = "095bd71214ae49faa799e64b8f65bce6";
+export const BUILDER_MODEL = "page";
+
+export default component$(() => {
+  const location = useLocation();
+  const builderContentRsrc = useResource$<any>(() => {
+    return getContent({
+      model: BUILDER_MODEL,
+      apiKey: BUILDER_PUBLIC_API_KEY,
+      options: getBuilderSearchParams(location.query),
+      userAttributes: {
+        urlPath: location.pathname || "/",
+      },
+    });
+  });
+
+  return (
+    <Resource
+      value={builderContentRsrc}
+      onPending={() => <div>Loading...</div>}
+      onResolved={(content) => (
+        <RenderContent
+          model={BUILDER_MODEL}
+          content={content}
+          apiKey={BUILDER_PUBLIC_API_KEY}
+        />
+      )}
+    />
+  );
+});
