1407ab699804066a29c5685a509acb5789d702c4
matthew harwood <matthhar12@gmail.com>
2023-02-24 19:31:33 -0800

    Added article api

--- src/routes/work/index.tsx
+++ src/routes/work/index.tsx
@@ -1,11 +1,23 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $} from "@builder.io/qwik";
+
+import { loader$ } from "@builder.io/qwik-city";
+import { builder } from '@builder.io/sdk'
+
+
+
+export const getWorks = loader$(async() => {
+  builder.init('bfe02904facd4583b37f3468b0850052')
+  const {data} = await builder.get('articles').promise()
+  return data;
+});
 
 export default component$(() => {
+  const data = getWorks();
 
   return (
-    <>
-
-    </>
+    <pre>
+      {JSON.stringify(data.value, null, 2)}
+    </pre>
 
   )
 });
